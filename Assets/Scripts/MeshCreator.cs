a26a7e327461f28c61a79d5d2927feae46ba5507
Dustman <dreed12@mail.pima.edu>
2018-09-06 18:52:29 -0700

    Made front face of cube

--- Assets/Scripts/MeshCreator.cs
+++ Assets/Scripts/MeshCreator.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MeshCreator : MonoBehaviour {
+public class MeshCreator : MonoBehaviour
+{
+    public MeshFilter filter;
+    public Vector3[] verts;
+    public int[] tris;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Start()
+    {
+        Mesh nMesh = new Mesh();
+        nMesh.vertices = verts;
+        nMesh.triangles = tris;
+
+        filter.mesh = nMesh;
+    }
+
+    private void Update()
+    {
+        
+    }
+
 }
