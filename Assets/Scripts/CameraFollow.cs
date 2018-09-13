19fc5498e649dde20e67dfb7c09e425cd3237269
Dustman <dreed12@mail.pima.edu>
2018-09-12 17:51:47 -0700

    worked on player movement and camera follow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,16 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CameraFollow : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform Target;
+    public float SmoothSpeed;
+    public Vector3 Offset;
+
+    private Vector3 desiredPosition;
+    private Vector3 smoothedPosition;
+
+
+    private void FixedUpdate()
+    {
+        desiredPosition = Target.position + Offset;
+        smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, SmoothSpeed);
+
+        transform.position = smoothedPosition;
+        transform.LookAt(Target);
+    }
+
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float Speed;
+    public float Turn;
+    public float JumpSpeed;
+    public float JumpHeight;
+    public Rigidbody Rigbod;
+
+    private float vert;
+    private float horiz;
+    private Vector3 movement;
+    private Vector3 turn;
+
+    private void Start()
+    {
+        movement = new Vector3(0, 0, 0);
+        turn = new Vector3(0, 0, 0);
+    }
+
+    private void Update()
+    {
+        vert = Input.GetAxis("Vertical");
+        horiz = Input.GetAxis("Horizontal");
+
+        movement = new Vector3( horiz, 0.0f, vert );
+       
+        Rigbod.AddForce(movement * Speed * Time.deltaTime);
+        if(Input.GetKeyDown("v"))
+        {
+            StopRolling();
+        }
+
+        if(Input.GetAxis("Jump")>0)
+        {
+
+            JumpUp();
+        }
+    }
+
+    private void StopRolling()
+    {
+        Rigbod.velocity = Vector3.zero;
+    }
+    private void JumpUp()
+    {
+        if(transform.position.y <= transform.position.y + JumpHeight)
+            Rigbod.AddForce(Vector3.up * JumpSpeed * Time.deltaTime);
+    }
+}
