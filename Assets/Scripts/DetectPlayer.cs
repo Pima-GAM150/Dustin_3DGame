f8a957ea97638564a640dcae2e1d2c15625a3f43
Dustman <dreed12@mail.pima.edu>
2018-09-20 19:41:24 -0700

    enemy follow work

--- Assets/Scripts/DetectPlayer.cs
+++ Assets/Scripts/DetectPlayer.cs
@@ -1,16 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.AI;
 
-public class DetectPlayer : MonoBehaviour {
+public class DetectPlayer : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject Player;
+    public float VisionRadius;
+    public NavMeshAgent NavMesh;
+    public Vector3 Offset;
+
+    private bool PlayerDetected;
+    private RaycastHit hit;
+
+    private void FixedUpdate()
+    {
+        if (NavMesh.isActiveAndEnabled)
+        {
+            if (Physics.Raycast(transform.position, Player.transform.position, out hit) && hit.distance <= VisionRadius )
+            {
+                NavMesh.SetDestination(Player.transform.position);
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!NavMesh.isActiveAndEnabled)
+            NavMesh.enabled = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (NavMesh.isActiveAndEnabled)
+        {
+            NavMesh.destination = Player.transform.position - Offset;
+        }
+    }
 }
