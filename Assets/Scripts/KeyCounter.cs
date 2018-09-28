5319801127999688e84956d6c54987df1ecbbb97
Dustman <dreed12@mail.pima.edu>
2018-09-27 17:47:06 -0700

    worked on spawn key and spawn enemy

--- Assets/Scripts/KeyCounter.cs
+++ Assets/Scripts/KeyCounter.cs
@@ -1,16 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KeyCounter : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int NumberOfKeys;
+    
+
+
+    private void Start()
+    {
+        NumberOfKeys = 0;
+    }
+
+    private void LateUpdate()
+    {
+        if (NumberOfKeys == 3)
+        {
+            SceneManager.LoadScene("3DgameLevel 2");
+        }
+        if(NumberOfKeys < 0 )
+        {
+            NumberOfKeys = 0;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ( collision.gameObject.tag == "Key")
+        {
+            NumberOfKeys++;
+        }
+        else if (collision.gameObject.tag == "Enemy")
+        {
+            NumberOfKeys--;
+        }
+        
+    }
 }
---
+++ Assets/Scripts/SpawnKeys.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnKeys : MonoBehaviour {
+
+    public GameObject Key;
+
+    Random rnd;
+    Vector3 pos;
+    Quaternion rot;
+
+	void Start ()
+    {
+        rot = Quaternion.identity;
+
+		for (int i = 0; i < 3; i++)
+        {
+            pos = new Vector3(Random.Range(120, 890), Random.Range(120, 890), 80);
+
+            Instantiate(Key, pos, rot);
+        }
+	}
+	
+}
